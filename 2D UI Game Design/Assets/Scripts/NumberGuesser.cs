ff4e31090378f5383e1affcb0b89d94b13f5afdd
Harley Garcia <hagarcia99@outlook.com>
2020-06-04 10:19:08 -0500

    Done messing around with ui

---
+++ 2D UI Game Design/Assets/Scripts/NumberGuesser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberGuesser : MonoBehaviour {
+
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    void Start () {
+        StartGame();
+	}
+	
+    void StartGame() {
+        NextGuess();
+    }
+
+    public void OnPressHigher() {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower() {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess() {
+        guess = Random.Range(min, max);
+        guessText.text = guess.ToString();
+    }
+}
